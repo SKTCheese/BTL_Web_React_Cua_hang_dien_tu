ea854f9f14e0e532077e947c9b94c8e58a189e34
Chiie <nguyenthimaichi248@gmail.com>
2026-03-23 11:28:35 +0700

    Quan ly san pham

--- client/src/components/admin/index.tsx
+++ client/src/components/admin/index.tsx
@@ -2,6 +2,5 @@ import DashboardAdmin from "./dashboardAdmin";
 import Categories from "./categories";
 import Products from "./products";
 import Orders from "./orders";
-
 export { DashboardAdmin, Categories, Products, Orders };
 
