0c678bb5a1a90e966741aaeef5986a1a2768c66b
Chiie <nguyenthimaichi248@gmail.com>
2026-03-31 08:46:51 +0700

    Xu ly order cua nguoi dung

---
+++ client/src/components/shop/order/FetchApi.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+const apiURL = "http://localhost:8000";
+
+export const getBrainTreeToken = async () => {
+  let uId = JSON.parse(localStorage.getItem("jwt")).user._id;
+  try {
+    let res = await axios.post(`${apiURL}/api/braintree/get-token`, {
+      uId: uId,
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createOrder = async (orderData) => {
+  try {
+    let res = await axios.post(`${apiURL}/api/order/create-order`, orderData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
