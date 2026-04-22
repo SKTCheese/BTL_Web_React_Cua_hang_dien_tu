396f2dfa968038069578a1dac324dc5fe0ea6928
Chiie <nguyenthimaichi248@gmail.com>
2026-04-22 13:04:45 +0700

    Gio hang cho shop

---
+++ client/src/components/shop/partials/CartModal.tsx
@@ -0,0 +1,36 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { LayoutContext } from "../index";
+import { cartListProduct } from "./FetchApi";
+import { isAuthenticate } from "../auth/fetchApi";
+import { cartList } from "../productDetails/Mixins";
+import { subTotal, quantity, totalCost } from "./Mixins";
+
+const apiURL = "http://localhost:8000";
+
+const CartModal = () => {
+  const history = useHistory();
+
+  const { data, dispatch } = useContext(LayoutContext);
+  const products = data.cartProduct;
+
+  const cartModalOpen = () =>
+    dispatch({ type: "cartModalToggle", payload: !data.cartModal });
+
+  useEffect(() => {
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      let responseData = await cartListProduct();
+      if (responseData && responseData.Products) {
+        dispatch({ type: "cartProduct", payload: responseData.Products });
+        dispatch({ type: "cartTotalCost", payload: totalCost() });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
